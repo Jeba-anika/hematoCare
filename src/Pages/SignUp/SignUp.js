892e6ca4d36701b028e7fa7f1e49fcd655e8d1b7
Jeba-anika <anikanur98@gmail.com>
2022-11-08 12:43:24 +0600

    login and signup page created

---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,94 @@
+import { Button, Label, TextInput } from 'flowbite-react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import login from '../../assets/login.png'
+
+import { FaGoogle, FaFacebook } from "react-icons/fa";
+
+const SignUp = () => {
+    const { register, handleSubmit } = useForm();
+
+    const handleSignUp = (data) => {
+        console.log(data)
+        const email = data.email;
+        const password = data.password;
+        console.log(email, password)
+    }
+
+    return (
+        <div className='flex flex-col lg:flex-row md:flex-row mr-2 ml-2 md:mr-20 md:ml-20 lg:mr-40 lg:ml-40 mt-20 rounded-xl p-8 md:p-10 lg:p-20 '>
+            <div className=''>
+                <img src={login} alt="" />
+            </div>
+            <div className='w-full mx-auto mt-5 md:mt-0 lg:mt-0'>
+                <form onSubmit={handleSubmit(handleSignUp)} className="flex flex-col gap-4 w-full ">
+                    
+                    <div className='lg:w-1/2 lg:mx-auto md:mx-auto'>
+                        <div className="mb-2 block text-start">
+                            <Label
+                                htmlFor="name"
+                                value="Your Name"
+                            />
+                        </div>
+                        <TextInput
+                            {...register('name')}
+                            id="name"
+                            type="text"
+                            placeholder="Your Name"
+                            required={true}
+                        />
+                    </div>
+                    <div className='lg:w-1/2 lg:mx-auto md:mx-auto'>
+                        <div className="mb-2 block text-start">
+                            <Label
+                                htmlFor="email1"
+                                value="Your email"
+                            />
+                        </div>
+                        <TextInput
+                            {...register('email')}
+                            id="email1"
+                            type="email"
+                            placeholder="Your Email"
+                            required={true}
+                        />
+                    </div>
+                    <div className='lg:w-1/2 lg:mx-auto md:mx-auto'>
+                        <div className="mb-2 block text-start">
+                            <Label
+                                htmlFor="password1"
+                                value="Your password"
+                            />
+                        </div>
+                        <TextInput
+                            {...register('password')}
+                            id="password1"
+                            type="password"
+                            placeholder='Your Password'
+                            required={true}
+                        />
+                    </div>
+
+                    <Button className='lg:w-1/2 lg:mx-auto md:w-1/2 md:mx-auto ' color='failure' pill={true} type="submit">
+                        Sign Up
+                    </Button>
+                </form>
+                <p className='mt-4'>Already have an account? Please <button className='hover:border-b font-bold hover:border-red-500'><Link to='/login'>Login</Link></button></p>
+                <hr className='w-1/2 mx-auto mt-3' />
+                <p className='font-bold mt-2 mb-2'>--Or Sign In with--</p>
+                <div className='mx-auto w-1/2 flex flex-col gap-4 md:flex-row md:gap-2 lg:gap-0 lg:flex-row justify-around'>
+                    <Button color="gray">
+                        <FaGoogle></FaGoogle><span className='ml-4'>Google</span>
+                    </Button>
+                    <Button color="gray">
+                        <FaFacebook></FaFacebook><span className='ml-4'>Facebook</span>
+                    </Button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,78 @@
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png'
+
+const Header = () => {
+    return (
+        <div >
+            <Navbar
+                className='bg-orange-200'
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand>
+                    <Link className='flex' to='/'>
+                    <img
+                        src={logo}
+                        className="mr-3 h-6 sm:h-9"
+                        alt="HematoCare Logo"
+                    />
+                    <p className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                        <span className='text-red-500'>Hemato</span><span>Care</span>
+                    </p>
+                    </Link>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Dropdown
+                        arrowIcon={false}
+                        inline={true}
+                        label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true} />}
+                    >
+                        <Dropdown.Header>
+                            <span className="block text-sm">
+                                Bonnie Green
+                            </span>
+                            <span className="block truncate text-sm font-medium">
+                                name@flowbite.com
+                            </span>
+                        </Dropdown.Header>
+                        <Dropdown.Item>
+                            Dashboard
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Settings
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            Earnings
+                        </Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item>
+                            Sign out
+                        </Dropdown.Item>
+                    </Dropdown>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link>
+                        <Link to='/'>Home</Link>
+                    </Navbar.Link>
+                    <Navbar.Link>
+                        <Link to='/login'>Login</Link>
+                    </Navbar.Link>
+                    <Navbar.Link>
+                        <Link to='/signup'>SignUp</Link>
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
